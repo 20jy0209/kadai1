2da833a7270d9733ecc59b85f664b0dc638ead82
20jy0209 <20jy0209@PC29.jynet2.jec.ac.jp>
2021-11-12 13:44:50 +0900

    右上文字色を青に変える

--- src/ez/DrawFigure.java
+++ src/ez/DrawFigure.java
@@ -39,7 +39,7 @@ public class DrawFigure {
 
 		//文字を描画する(文字列の中心座標x, y, 文字列，色 [,サイズ] )
 		EZ.addText(40,20, "Hello World", Color.BLACK, 12);
-		EZ.addText(300,50, "012あいう漢字", Color.BLACK, 24);
+		EZ.addText(300,50, "012あいう漢字", Color.BLUE, 24);
 
 		// 円を描く．(中心座標x, y, 幅，高さ，色，塗りつぶし)
 		EZ.addCircle(100, 100, 150, 150, Color.GREEN, true);
